4f6be59b330f4794c86e6982e76c8607047ca91a
denishamann@gmail.com <denishamann@gmail.com@f0451d12-e048-e25c-0463-b4346ca5d719>
2013-11-23 14:51:55 +0000

    *ADD: -initial ruby on rails project commit

---
+++ trunk/src/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GeogameProject::Application.config.secret_key_base = '802a1a83067cd3ed85dbfbea98c20b9efbf90ec8bf009603550d1e51e525bbc34f8405b7368470024f4a8c3d270aa626b7fec00f7838f8c6ed2f3ab4e679bfe5'
