2feab216b6ae08425b46b74977e846ec41040ba2
denishamann@gmail.com <denishamann@gmail.com@f0451d12-e048-e25c-0463-b4346ca5d719>
2014-01-22 17:23:19 +0000

    *ADD: -each user has flags -a flag belongs to  a user (plural fix)

--- trunk/src/app/views/flags/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@flags) do |flag|
-  json.extract! flag, :owner, :prestige
-  json.url flag_url(flag, format: :json)
-end
--- trunk/src/config/initializers/leaftlet.rb
+++ trunk/src/config/initializers/leaftlet.rb
@@ -1,4 +1,4 @@
-Leaflet.tile_layer = "http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png"
+Leaflet.tile_layer = "http://a.tile.openstreetmap.org/{z}/{x}/{y}.png"
 # You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
 Leaflet.attribution = "OSM - Mapquest"
 Leaflet.max_zoom = 18
